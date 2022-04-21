a92f6805dd9cdaaed11a1736ea07f503809ed4e1
faeefa <101559335+faeefa@users.noreply.github.com>
2022-04-21 08:23:54 -1000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,6 @@ int main(void)
         char r3[11];
         char *res;
 
-        res = infinite_add(n, m, r, 100);
         if (res == 0)
         {
                 printf("Error\n");
@@ -25,7 +24,7 @@ int main(void)
         }
         n = "1234567890";
         m = "1";
-        res = infinite_add(n, m, r2, 10);
+        
         if (res == 0)
         {
                 printf("Error\n");
@@ -36,7 +35,6 @@ int main(void)
         }
         n = "999999999";
         m = "1";
-        res = infinite_add(n, m, r2, 10);
         if (res == 0)
         {
                 printf("Error\n");
@@ -45,7 +43,7 @@ int main(void)
         {
                 printf("%s + %s = %s\n", n, m, res);
         }
-        res = infinite_add(n, m, r3, 11);
+        
         if (res == 0)
         {
                 printf("Error\n");
